1cc7eb9c03d0f5bf48aecf7c737af1fe3d625595
shivam240104 <mauryatanmay845@gmail.com>
2024-06-24 19:24:21 +0530

    add description

--- src/productTab.jsx
+++ src/productTab.jsx
@@ -4,11 +4,12 @@ import Price from "./Price"
 export default function productTab({title, idx, price, features}){
 let oldPrice=["12,495", "11,900", "1,599", "599"];
 let newPrice=["8,995", "9,199", "899", "278"];
+let description =["8,000 DPI","intuitive surface",  "desingned for iPad Pro", "wireless"];
 
     return(
        <div className="product">
         <h4>{title}</h4>
-        <p>Description</p>
+        <p>{description[idx]}</p>
         <Price oldPrice={oldPrice[idx]} newPrice={newPrice[idx]} />
        </div> 
     )
