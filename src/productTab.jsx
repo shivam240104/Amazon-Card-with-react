5e402ec6b6b43310f1a2759e18976b48898c5e32
shivam240104 <mauryatanmay845@gmail.com>
2024-06-24 19:55:29 +0530

    add description with help of 2d array

--- src/productTab.jsx
+++ src/productTab.jsx
@@ -4,12 +4,13 @@ import Price from "./Price"
 export default function productTab({title, idx, price, features}){
 let oldPrice=["12,495", "11,900", "1,599", "599"];
 let newPrice=["8,995", "9,199", "899", "278"];
-let description =["8,000 DPI","intuitive surface",  "desingned for iPad Pro", "wireless"];
+let description =[["8,000 DPI","5 Programmable buttons"],["intuitive surface","desingned for iPad Pro"],  ["desingned for iPad Pro","desingned for iPad Pro"], ["wireless","desingned for iPad Pro"]];
 
     return(
        <div className="product">
         <h4>{title}</h4>
-        <p>{description[idx]}</p>
+        <p>{description[idx][0]}</p>
+        <p>{description[idx][1]}</p>
         <Price oldPrice={oldPrice[idx]} newPrice={newPrice[idx]} />
        </div> 
     )
