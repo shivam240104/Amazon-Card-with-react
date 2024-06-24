351674207acc4aca43b13fc1dc4a503b6b29664b
shivam240104 <mauryatanmay845@gmail.com>
2024-06-24 18:26:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import ProductTab from './productTab'
+
+function App() {
+  
+
+
+  return (
+    <>
+     
+    <ProductTab/>
+    <ProductTab/>
+    <ProductTab/>
+    <ProductTab/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/productTab.jsx
@@ -0,0 +1,7 @@
+export default function productTab(){
+    return(
+       <div className="product">
+        <h1>Product</h1>
+       </div> 
+    )
+}
