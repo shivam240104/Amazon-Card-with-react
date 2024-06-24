416e5c587644e77be093e6ea4c88719d6df96fc5
shivam240104 <mauryatanmay845@gmail.com>
2024-06-24 19:31:06 +0530

    giving style in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import './App.css'
 import ProductTab from './productTab'
 
 function App() {
+ let style = {
+  display:"flex",
+  flexWrap:"wrap",
+  justifyContent:"center",
+  alignItems:"center",
+ }
+
   return (
-    <>
-    <ProductTab title="Logitech MX Master" idx={0} />
-    <ProductTab title="Apple Pencil (2nd Gen" idx={1} />
-    <ProductTab title="Zebronics Zeb-transformer" idx={2} />
-    <ProductTab title="Petronics Toad 23" idx={3} />
+    <div style={style} >
+        <ProductTab title="Logitech MX Master" idx={0} />
+            <ProductTab title="Apple Pencil (2nd Gen" idx={1} />
+            <ProductTab title="Zebronics Zeb-transformer" idx={2} />
+            <ProductTab title="Petronics Toad 23" idx={3} />
+    </div>
+   
     
-    </>
+    
   )
 }
 
